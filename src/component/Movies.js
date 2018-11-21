6b849e221ab5fd8e2c3e3a50bfe8de927343ed5e
Md.Ayub Khan <itsaiub@gmail.com>
2018-11-21 16:19:14 +0600

    get movie from db and show error msg

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { getMovies } from "../services/fakeMovieService";
+import { toast } from "react-toastify";
+import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import Pagination from "./common/Pagination";
 import ListGroup from "./common/ListGroup";
@@ -23,12 +24,24 @@ class Movies extends Component {
   async componentDidMount() {
     const { data } = await getGenres();
     const genres = [{ name: "All Genres", _id: "" }, ...data];
-    this.setState({ movies: getMovies(), genres });
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({ movies: movies });
+  handleDelete = async movie => {
+    const originalMovies = this.state.movies;
+    const movies = originalMovies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        toast.error("This movie has already been deleted.");
+        this.setState({ movies: originalMovies });
+      }
+    }
   };
 
   handleLike = movie => {
